a5ed963a9eb1f11aa8b49dc2c6b1edbff650f8e2
Fendamear <Nickvdmeer2001@gmail.com>
2023-03-16 14:15:21 +0100

    docker compose werkt voor alle containers

---
+++ Backend/KweetService/Program.cs
@@ -0,0 +1,57 @@
+using Kweet.Data;
+using Kweet.Services.Kweet;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddCors();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version(5, 7, 31)));
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddScoped<IKweetService, KweetService>();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Administrator", policy => policy.RequireClaim("user_roles", "[Administrator]"));
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, o =>
+            {
+                o.MetadataAddress = "https://<your-keycloak-server>/realms/<your-realm>/.well-known/openid-configuration";
+                o.Authority = "https://<your-keycloak-server>/realms/<your-realm>";
+                o.Audience = "account";
+            });
+
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<DataContext>();
+    context.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
