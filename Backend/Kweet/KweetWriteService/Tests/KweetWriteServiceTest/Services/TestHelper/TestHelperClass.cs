abc4b9807c2404cb21093dd4a2a209541ac331bc
Fendamear <Nickvdmeer2001@gmail.com>
2023-05-17 15:36:33 +0200

    first try build + test kweetWriteService

---
+++ Backend/Kweet/KweetWriteService/Tests/KweetWriteServiceTest/Services/TestHelper/TestHelperClass.cs
@@ -0,0 +1,42 @@
+﻿using Kweet.Models;
+using KweetWriteService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KweetServiceTest.Services.TestHelper
+{
+    public static class TestHelperClass
+    {
+        public static List<KweetModel> GetFakeKweetsList()
+        {
+            return new List<KweetModel>()
+            {
+            new KweetModel("Test Message 1", "User1"),
+            new KweetModel("Test Message 2", "User2"),
+            new KweetModel("Test Message 3", "User3"),
+            new KweetModel("Test Message 4", "User123")
+            };
+        }
+
+        public static List<LikeModel> GetFakeLikeList()
+        {
+            return new List<LikeModel>()
+            {
+                new LikeModel()
+            };
+        }
+
+        public static List<ReactionKweetModel> GetFakeReactionList()
+        {
+            return new List<ReactionKweetModel>()
+            {
+                new ReactionKweetModel()
+            };
+        }
+    }
+
+}
---
+++ Backend/Kweet/KweetWriteServiceTest/Services/Kweet/KweetServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kweet.Services.Kweet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+using KweetServiceTest.Services.TestHelper;
+using MockQueryable.EntityFrameworkCore;
+using Moq.EntityFrameworkCore;
+using Bogus;
+using Kweet.Data;
+using Kweet.Models;
+
+namespace Kweet.Services.Kweet.Tests
+{
+    [TestClass()]
+    public class KweetServiceTest
+    {
+        private KweetService _kweetService;
+        private Mock<DataContext> _dataContextMock;
+        private Mock<DbSet<KweetModel>> _mockSet;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _dataContextMock = new Mock<DataContext>();
+            _mockSet = new Mock<DbSet<KweetModel>>();
+        }
+
+        [TestMethod()]
+        public async Task getAllKweetsTest()
+        {
+            _dataContextMock.Setup(x => x.Kweets).ReturnsDbSet(TestHelperClass.GetFakeKweetsList());
+            _dataContextMock.Setup(x => x.Likes).ReturnsDbSet(TestHelperClass.GetFakeLikeList());
+            _kweetService = new KweetService(_dataContextMock.Object);
+
+            // Arrange
+            var userId = "user123";
+
+            // Act
+            var result = await _kweetService.getAllKweets(userId);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(4, result.Count);
+        }
+    }
+}
