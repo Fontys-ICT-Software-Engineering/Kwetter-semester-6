c09e841bf2386646111d65090c236b80e0f79f46
Fendamear <Nickvdmeer2001@gmail.com>
2023-05-20 15:35:50 +0200

    updated K8S

--- K8s/deployment.ps1
+++ K8s/deployment.ps1
@@ -5,7 +5,7 @@
 kubectl apply -f .\Authservice\authservice-db-deployment.yaml
 kubectl apply -f .\KweetReadService\mongodb-deployment.yaml
 kubectl apply -f .\KweetWriteService\KweetWriteservice-db-deployment.yaml
-kubectl apply -f .\ProfileService\ProfileService-db-deployment.yaml
+# kubectl apply -f .\ProfileService\ProfileService-db-deployment.yaml
 
 Start-Sleep -Seconds 35
 
@@ -18,7 +18,7 @@ Start-Sleep -Seconds 30
 # Creation of the microservices itself
 
 # creation of the messagebus queus
-kubectl apply -f .\ProfileService\Profileservice-deployment.yaml
+# kubectl apply -f .\ProfileService\Profileservice-deployment.yaml
 kubectl apply -f .\KweetReadService\kweetread-deployment.yaml
 
 Start-Sleep -Seconds 10
@@ -27,9 +27,6 @@ kubectl apply -f .\Authservice\authservice-deployment.yaml
 kubectl apply -f .\KweetWriteService\kweetwriteService-deployment.yaml
 
 # adding of management tools
-
-kubectl apply -f .\Overig\mongo-express-deployment.yaml
-kubectl apply -f .\Overig\phpmyadmin-deployment.yaml
 
 # configuring ingress controller
 
